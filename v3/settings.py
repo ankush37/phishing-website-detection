3c0c0120368bbf608e4fd1084c4f5ea777a335a3
Ankush Chauhan <chauhanankush37@gmail.com>
2023-07-23 00:07:26 +0530

    final 2

--- v3/settings.py
+++ v3/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!cz!*83_sip+h-t)m6ra0j&4akx&&5=)!(--jwr(c-217#os81'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["phishing-website-detection-production.up.railway.app"]
 
@@ -130,7 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
