6e5aa2fe81486416ff30b5ddba0fec43d20e8707
Ankush Chauhan <chauhanankush37@gmail.com>
2023-07-23 00:10:59 +0530

    final 2

--- v3/settings.py
+++ v3/settings.py
@@ -125,7 +125,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+CSRF_TRUSTED_ORIGINS = ['https://phishing-website-detection-production.up.railway.app']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
