fb93a7f092e637f0654d7ead97eacb8d2279b0ca
Ankush Chauhan <chauhanankush37@gmail.com>
2023-08-05 13:11:32 +0530

    fixed ssl checker

--- ml/model/ssl_checker.py
+++ ml/model/ssl_checker.py
@@ -12,27 +12,34 @@ def get_certificate(hostname, port):
     hostname_idna = idna.encode(hostname)
     sock = socket()
 
-    sock.connect((hostname, port))
-    peername = sock.getpeername()
-    ctx = SSL.Context(SSL.SSLv23_METHOD) # most compatible
-    ctx.check_hostname = False
-    ctx.verify_mode = SSL.VERIFY_NONE
+    try:
+        sock.connect((hostname, port))
+        peername = sock.getpeername()
+        ctx = SSL.Context(SSL.SSLv23_METHOD) # most compatible
+        ctx.check_hostname = False
+        ctx.verify_mode = SSL.VERIFY_NONE
 
-    sock_ssl = SSL.Connection(ctx, sock)
-    sock_ssl.set_connect_state()
-    sock_ssl.set_tlsext_host_name(hostname_idna)
-    sock_ssl.do_handshake()
-    cert = sock_ssl.get_peer_certificate()
-    # crypto_cert = cert.to_cryptography()
-    sock_ssl.close()
-    sock.close()
+        sock_ssl = SSL.Connection(ctx, sock)
+        sock_ssl.set_connect_state()
+        sock_ssl.set_tlsext_host_name(hostname_idna)
+        sock_ssl.do_handshake()
+        cert = sock_ssl.get_peer_certificate()
+        # crypto_cert = cert.to_cryptography()
+        sock_ssl.close()
+        sock.close()
 
-    return HostInfo(cert=cert, peername=peername, hostname=hostname)
+        return HostInfo(cert=cert, peername=peername, hostname=hostname)
+    except:
+        return -1
+    
 
 def verify_cert(hostname):
-    hostInfo = get_certificate(hostname, 443)
-    cert = hostInfo.cert
-    return not cert.has_expired()
+    try:
+        hostInfo = get_certificate(hostname, 443)
+        cert = hostInfo.cert
+        return not cert.has_expired()
+    except:
+        return -1
 
 
 # print(verify_cert("google.com"))
